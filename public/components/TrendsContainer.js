c5d43d037de897de09e9a6d557b2e071b5e6cc0a
Glitch (twitter-hide-replies-annotations-v2) <none>
2021-03-15 23:53:22 +0000

    🏘🍳 Checkpoint ./public/components/TrendsContainer.js:2481320/1926

--- public/components/TrendsContainer.js
+++ public/components/TrendsContainer.js
@@ -9,20 +9,45 @@ class TrendsContainer extends Emitter {
   
   getInitialState() {
     return {
-      queries: Emitter.registry.get(document.querySelector('main.tweet')).props.tweet.countsQuery,
-      done: {}
+      done: []
     };
   }
   
-  didReceiveData(response) {
+  async didReceiveData(response) {
     if (!response.url.match(/\/counts/)) {
+      return;
+    }
+    
+    if (!response.ok) {
       return;
     }
     
     const url = new URL(response.url);
     const query = url.searchParams.get('q');
-    const stats
+    if (!query) {
+      return;
+    }
+
+    const { name } = this.props.queries.find(q => q.query === query );
+    if (!name) {
+      return;
+    }
+
+    const done = this.state.done;
+    done.push({query: query, name: name, stats: await response.clone().json()});
+    this.setState({done: done});
   }
   
-  render() {}
+  render() {
+    this.state.done.forEach(stat => {
+      if (document.querySelector(`[data-query="${stat.query}"]`)) {
+        return;
+      }
+      
+      const bigNumber = document.querySelector('[e\\:class="BigNumber"]').cloneNode(true);
+      big
+      bigNumber.props.stats = stat.stats
+      
+    })    
+  }
 }
