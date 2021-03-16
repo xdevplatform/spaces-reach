e1b5cc806501475601f60f8efa77d517ba6f2853
Glitch (twitter-hide-replies-annotations-v2) <none>
2021-03-16 06:02:05 +0000

    😅🚖 Checkpoint ./public/components/Chart.js:2481320/531 ./public/style.css:2481320/23 ./views/trends.html:2481320/270

--- public/components/Chart.js
+++ public/components/Chart.js
@@ -5,9 +5,16 @@ class Chart extends Emitter {
   
   render() {
     try {
+      this.component.innerHTML = '';
       const data = JSON.parse(this.component.dataset.data);
-      const max = Math.max(...data.map(data => data.count));
-      data.map
+      const dataCounts = data.map(data => data.count);
+      const max = Math.max(...dataCounts);
+      dataCounts.map(count => {
+        const histogram = document.createElement('div');
+        const height = (count / max) * 100;
+        histogram.style.height = height.toFixed(2) + '%';
+        this.component.appendChild(histogram);
+      });
     } catch(e) {
       return;
     }
