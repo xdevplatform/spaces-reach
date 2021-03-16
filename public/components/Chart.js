a34eca889462dfe0ea178c477227c12a0d7bbc5e
Glitch (twitter-hide-replies-annotations-v2) <none>
2021-03-16 17:04:44 +0000

    ⏱🛢 Checkpoint ./public/style.css:2481320/160 ./public/components/Chart.js:2481320/749

--- public/components/Chart.js
+++ public/components/Chart.js
@@ -1,9 +1,11 @@
 class Chart extends Emitter {
   constructor(element) {
     super(element);
-    if (this.component.dataset.data) {
+    try {
       const data = JSON.parse(this.component.dataset.data);
       this.dataCounts = data.map(data => data.count);      
+    } catch (e) {
+      console.error(e);
     }
   }
   
@@ -11,31 +13,31 @@ class Chart extends Emitter {
     return this.dataCounts;
   }
   
-  determineTrend() {
-    // datacounts.length * sumOfMultipliedValues - sumOfLength * sumOfValues / doatacounts.length * sumOfSquares - sqrt(sumOfLength)
-    
+  determineTrend() {   
     const length = this.dataCounts.length;
     let sumOfMultipliedValues = this.dataCounts.reduce((ac, el, i) => ac + el * i);
     let sumOfLength = this.dataCounts.reduce((ac, el, i) => ac + i);
     let sumOfValues = this.dataCounts.reduce((ac, el) => ac + el);
     let sumOfSquares = this.dataCounts.reduce((ac, el, i) => ac + el ** i);
     
-
-    return length * sumOfMultipliedValues - sumOfLength * sumOfValues / length * sumOfSquares - Math.sqrf(sumOfLength);
+    return length * sumOfMultipliedValues - sumOfLength * sumOfValues / length * sumOfSquares - Math.sqrt(sumOfLength);
   }
   
   render() {
-    try {
-      this.component.innerHTML = '';
-      const max = Math.max(...this.dataCounts);
-      this.dataCounts.map(count => {
-        const histogram = document.createElement('div');
-        const height = (count / max) * 100;
-        histogram.style.height = height.toFixed(2) + '%';
-        this.component.appendChild(histogram);
-      });
-    } catch(e) {
-      return;
+    this.component.innerHTML = '';
+    const max = Math.max(...this.dataCounts);
+    this.dataCounts.map(count => {
+      const histogram = document.createElement('div');
+      const height = (count / max) * 100;
+      histogram.style.height = height.toFixed(2) + '%';
+      this.component.appendChild(histogram);
+    });
+    
+    const trend = this.determineTrend();
+    if (trend < 0) {
+      this.component.classList.add('down');
+    } else if (trend > 0) {
+      this.component.classList.add('up');
     }
   }
 }
