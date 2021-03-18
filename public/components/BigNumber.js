019ab43149697bd781c29599a9a32f89e6d5fd5e
Glitch (twitter-hide-replies-annotations-v2) <none>
2021-03-18 00:28:49 +0000

    🏦🎨 Checkpoint ./public/components/BigNumber.js:2481320/720 ./views/trends.html:2481320/17

--- public/components/BigNumber.js
+++ public/components/BigNumber.js
@@ -2,22 +2,35 @@ class BigNumber extends Emitter {
   constructor(element) {
     super(element);
     this.title = element.querySelector('h2');
-    this.number = element.querySelector('h4');
+    this.number = element.querySelector('h4.trend');
+    this.loading = element.querySelector('h4.loading');
+    
     this.chart = this.childNodes()[0];
   }
   
   async didReceiveData(response) {
+    console.log(response.url);
+    const regex = new RegExp(`/counts\?${this.component.dataset.search}`);
+    if (!response.url.match(/\/counts\/\?))
     try {
-    const json = await response.clone().json();
+      const json = await response.clone().json();
       this.setState(json);
     } catch (e) {
       this.setState({error: true});
     }
-
+  }
+  
+  fetch() {
+    Emitter.dispatch(fetch(`/counts?q=${this.component.dataset.search}`));
+    this.loading.innerText = 'Loading…';
+    this.loading.hidden = false;
+    Emitter.dispatch(fetch)
   }
     
   render() {
-    
+    if (this.state.error) {
+      
+    }
     
     this.title.innerText = this.component.dataset.name;
     this.chart.dataset.data = this.component.dataset.results;
