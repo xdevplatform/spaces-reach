a70c69fb444bae88067e70e82ed0dfbeed765982
Daniele Bernardi <dbernardi@twitter.com>
2021-08-17 12:16:23 -0700

    remove headers

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 export const headers = {
   authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
-  'x-des-apiservices': 'staging1',
-  'X-Decider-Overrides': 'tfe_route:des_apiservice_staging1=on'
 }
